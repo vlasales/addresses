463069c00231b625a09e532795793f5a741c2335
Kit <Kit@Callisto>
2018-04-16 11:35:45 +0200

    Send form with method POST

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,31 @@ use Psr\Http\Message\ResponseInterface as Response;
  * - you can use $this->database which is an instance of MongoDB\Database and $this->logger that represents Monolog\Logger
  */
 
-$app->post('/address', function(Request $request, Response $response) {});
+$app->post('/address', function(Request $request, Response $response, array $args) {
+	$response->getBody()->write("<pre>" . var_dump($args) . "</pre>");
+	return $response;
+});
 $app->patch('/address/{id}', function(Request $request, Response $response) {});
 $app->delete('/address/{id}', function(Request $request, Response $response) {});
-$app->get('/address[/{id}]', function(Request $request, Response $response) {});
+$app->get('/address[/{id}]', function(Request $request, Response $response) {
+	$response->getBody()->write("Nazdar");
+	return $response;
+});
+$app->get('/', function(Request $request, Response $response) {
+	$response->getBody()->write(<<<EOT
+	<p><a href="/address">Returns all available address</a></p>
+	<form action="/address" method="POST">
+		<ul>
+			<li><input type="text" name="country" value="CZ"></li>
+			<li><input type="text" name="city" value="Brno"></li>
+			<li><input type="text" name="Street" value="Husova"></li>
+			<li><input type="text" name="postalcode" value="60200"></li>
+			<li><input type="text" name="number" value="6"></li>
+			<li><input type="text" name="numberAddition"></li>
+			<li><button name="newAddress" value="Send">Send new address"</button></li>
+		</ul>
+	</form>
+EOT
+			);
+   return $response;
+});
